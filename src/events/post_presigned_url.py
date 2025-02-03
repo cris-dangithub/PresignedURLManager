a3a36bc2d2da6e837d386f6324d145162bb47cc9
cris-dangithub <cristiandaniel8080@gmail.com>
2025-02-02 19:42:13 -0500

    refactor: response

--- src/events/post_presigned_url.py
+++ src/events/post_presigned_url.py
@@ -6,11 +6,6 @@ def handle_post_presigned_url(event):
     bucket_name = event.get("bucket_name")
     object_key = event.get("object_key")
     expiration = event.get("expiration", 60)
-    #  xlsx contentype
-    content_type = event.get(
-        "content_type",
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    )
 
     if not bucket_name or not object_key:
         return {
@@ -19,10 +14,10 @@ def handle_post_presigned_url(event):
         }
 
     s3_client = get_s3_client()
-    url = s3_client.generate_presigned_post(
+    response = s3_client.generate_presigned_post(
         bucket_name,
         object_key,
         ExpiresIn=expiration,
     )
 
-    return {"statusCode": 200, "body": json.dumps({"url": url})}
+    return {"statusCode": 200, "body": json.dumps({**response})}
